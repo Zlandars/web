1f04c881ded9ee1766cab2816ab5e789b108f504
Evgeniy Landarsky <mag20102009@gmail.com>
2020-10-15 22:37:11 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Запуск при загрузке страницы.
+// function runScript() {
+//     var inputWithNumber1 = document.getElementById("//id")
+//     //or
+//     var number1 = getNumberFromHtml("//id")
+//     var sum = getElementByTagName("")
+// }
+
+// window.onload = runScript;
