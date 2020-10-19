4cafc74ae50a92707d683c424761b069842957d8
Evgeniy Landarsky <mag20102009@gmail.com>
2020-10-19 16:44:23 +0300

    Scss and js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@
 // }
 
 // window.onload = runScript;
+
+/* Установите ширину боковой навигации 250 пикселей, и левое поле содержимого страницы - 250 пикселей и добавьте черный цвет фона к телу. */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("closewin").style.position = "fixed";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Установите ширину боковой навигации равной 0, и левое поле содержимого страницы - 0, и цвет фона тела - белым. */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("closewin").style.position = "absolute";
+    document.body.style.backgroundColor = "white";
+  }
